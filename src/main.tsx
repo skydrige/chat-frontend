b3b6d8d42999a24ec8b7a009ab1fc90bd06818d9
skydrige <klokeshklokesh42376@gmail.com>
2024-06-26 15:19:34 +0530

    Initial

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// @ts-ignore
+import './components/analytics';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
