c1c8a83f1ec52f7bc7c223c24511340fecd3fe2e
skydrige <klokeshklokesh42376@gmail.com>
2024-06-27 10:10:27 +0530

    Initial

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,10 +1,9 @@
-// components/ChatInput.tsx
 import React from 'react';
-import '../assets/css/App.css';
 import { BsSend } from "react-icons/bs";
 import {Button, Form, InputGroup} from "react-bootstrap";
 
 const ChatInput: React.FC = () => {
+
 	return (
 		<div className="chat-input-section p-2 m-2">
 			<InputGroup className={"chat-input"}>
@@ -14,8 +13,8 @@ const ChatInput: React.FC = () => {
 					aria-describedby="basic-addon2"
 					className={"chat-input-field"}
 				/>
-				<InputGroup.Text className={"p-0 me-2"} style={{ backgroundColor: "inherit" }}>
-					<Button className={"chat-button p-0 me-2 ms-2"} style={{ backgroundColor: "inherit", border: "none" }}>
+				<InputGroup.Text className={"chat-input-group"}>
+					<Button className={"chat-button"}>
 						<BsSend />
 					</Button>
 				</InputGroup.Text>
