d9b6f6a476b872f1e4587557be8b07ad0e701f22
skydrige <klokeshklokesh42376@gmail.com>
2024-06-27 11:31:12 +0530

    Initial

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -6,12 +6,13 @@ const ChatInput: React.FC = () => {
 
 	return (
 		<div className="chat-input-section p-2 m-2">
-			<InputGroup className={"chat-input"}>
+			<InputGroup className={"chat-input"} id={"chat-input-field"}>
 				<Form.Control
+					type={"text"}
 					placeholder="Type a message..."
 					aria-label="Recipient's username"
 					aria-describedby="basic-addon2"
-					className={"chat-input-field"}
+					className={"chat-input"}
 				/>
 				<InputGroup.Text className={"chat-input-group"}>
 					<Button className={"chat-button"}>
