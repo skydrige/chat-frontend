aa7718d4f2e648f0711c9f759cf6cbe72666fbb6
skydrige <klokeshklokesh42376@gmail.com>
2024-06-29 10:28:10 +0530

    Reconstruct chat-input

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 import { BsSend } from 'react-icons/bs';
 import { Button, Col, Row } from 'react-bootstrap';
 import '../assets/css/App.css'; // Import your CSS file
+import { LuFileSymlink } from "react-icons/lu";
 
 const ChatInput: React.FC = () => {
 	return (
 		<div className="chat-input-section">
 			<Row className="chat-input-field">
+				<Col xs={1} className={"file-link"}>
+					<Button size={"lg"}>
+						<LuFileSymlink/>
+					</Button>
+				</Col>
 				<Col xs={8} className="chat-input">
 					<textarea placeholder="Type to Chat..." />
 				</Col>
