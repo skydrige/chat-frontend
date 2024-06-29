fbfb6474c1ea427b639f4bb62b965fe4aa48d832
skydrige <klokeshklokesh42376@gmail.com>
2024-06-29 22:53:49 +0530

    Background Improvement

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -5,7 +5,7 @@ import '../assets/css/App.css';
 const ChatArea: React.FC = () => {
 	return (
 		<div className="chat-section p-2 m-2">
-			<center>Chat Area</center>
+			<h3>Chat Area</h3>
 		</div>
 	);
 };
