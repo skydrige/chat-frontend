ff28728abed9cb29ede42c335e2d4dd8add73489
skydrige <klokeshklokesh42376@gmail.com>
2024-08-10 16:45:02 +0530

    Sidebar Footer

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,8 +10,20 @@ const Sidebar: React.FC = () => {
 				<Button>New Chat</Button>
 			</div>
 			<div className={"sidebar-content"}>
-				<Button>Home</Button>
-				<Button>Profile</Button>
+				<Button style={{ marginTop: "8px" }}>Chat - 1</Button>
+				<Button>Chat - 2</Button>
+				<Button>Chat - 3</Button>
+				<Button>Chat - 4</Button>
+				<Button>Chat - 5</Button>
+				<Button>Chat - 6</Button>
+				<Button>Chat - 7</Button>
+				<Button>Chat - 8</Button>
+				<Button>Chat - 9</Button>
+				<Button>Chat - 10</Button>
+				<Button>Chat - 11</Button>
+				<Button>Chat - 12</Button>
+			</div>
+			<div className={"sidebar-footer"}>
 				<Button>Settings</Button>
 				<Button>Logout</Button>
 			</div>
