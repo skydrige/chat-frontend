bddeb9be23341d3f3aa7504e51b06aa7f306cedc
skydrige <klokeshklokesh42376@gmail.com>
2024-08-10 14:58:16 +0530

    Avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Navbar, Button } from 'react-bootstrap';
 import sidebarIcon from '../assets/sidebar.png';
 import '../assets/css/App.css';
-import { Link } from "react-router-dom"; // Step 1: Import Link
+import { Link } from "react-router-dom";
+import Sungjinwoo from '../assets/SungJinwoo.png';
 
 interface NavbarProps {
 	onToggleSidebar: () => void;
@@ -22,6 +23,9 @@ const NavbarComponent: React.FC<NavbarProps> = ({ onToggleSidebar, isRotated }) 
 			<Link to="/home" style={{ textDecoration: 'none' }}>
 				<Navbar.Brand className="navbar-brand ms-3">Chat App</Navbar.Brand>
 			</Link>
+			<div className="ms-auto m-1">
+				<img className={"avatar rounded-circle me-3"} style={{ width: "30px", height: "30px" }} src={Sungjinwoo} alt={"Avatar"} />
+			</div>
 		</Navbar>
 	);
 };
